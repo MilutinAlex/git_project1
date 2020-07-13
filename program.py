9e60629e16dfceccfbc5e72c080fb539e5a623ed
MilutinAlex <68244599+MilutinAlex@users.noreply.github.com>
2020-07-13 20:46:19 +0300

    Еще один коммит

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('Еще одна строка')
+    print('Строка через Web-интерфейс')
 
 if __name__ == '__main__':
     main()
